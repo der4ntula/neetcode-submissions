3f85dd9c7d463e4f73953e729bec4768b711e075
Deran Akın <119085085+der4ntula@users.noreply.github.com>
2026-08-28 19:10:04 +0300

    Add: is-anagram - submission-1

---
+++ Data Structures & Algorithms/is-anagram/submission-1.py
@@ -0,0 +1,36 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        # we should iterate through the word and record
+        # how many times each letter appears into a hash map
+
+        # if the two hash maps are equal to each other,
+        # the words are anagrams of each other
+
+        # s = "racecar"
+        # t = "carrace"
+
+        # sMap = {r:2, a:2, c:2, e:1}
+        # tMap = {c:2, a:2, r:2, e:1}
+
+        if len(s) != len(t): # crucical step to avoid unnecessary iterations
+            return False
+
+        sMap = {}
+        tMap = {}
+
+        for letter in s:
+            if letter not in sMap:
+                sMap[letter]=1
+            else:
+                sMap[letter]+=1
+        
+        for letter in t:
+            if letter not in tMap:
+                tMap[letter]=1
+            else:
+                tMap[letter]+=1
+
+        if sMap == tMap:
+            return True
+        else:
+            return False
